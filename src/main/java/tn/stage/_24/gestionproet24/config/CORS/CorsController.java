1af5a783a01b0095fd156a1904a9ee43786c024c
Wael-BR <waelbenrejeb.wbr@gmail.com>
2024-08-15 12:12:11 +0100

    cors update

--- src/main/java/tn/stage/_24/gestionproet24/config/CORS/CorsController.java
+++ src/main/java/tn/stage/_24/gestionproet24/config/CORS/CorsController.java
@@ -1,8 +1,8 @@
 package tn.stage._24.gestionproet24.config.CORS;
 
 import jakarta.servlet.http.HttpServletResponse;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -10,7 +10,7 @@ public class CorsController {
 
     @RequestMapping(value = "/**", method = RequestMethod.OPTIONS)
     public void options(HttpServletResponse response) {
-        response.setHeader("Access-Control-Allow-Origin", "http://127.0.0.1:4200");
+        response.setHeader("Access-Control-Allow-Origin", "http://127.0.0.1:4200"); // Ensure this matches your Angular app URL
         response.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
         response.setHeader("Access-Control-Allow-Headers", "Authorization, Content-Type, X-Requested-With, Accept, Origin");
         response.setHeader("Access-Control-Allow-Credentials", "true");
